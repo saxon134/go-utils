5e8129a15a787086459be4a8f36fe54c31d104bb
saxon <jiangruihuang@126.com>
2022-12-14 12:17:18 +0800

    no message

--- saMicro/server.go
+++ saMicro/server.go
@@ -19,6 +19,8 @@ func NewService() Service {
 	return server
 }
 
+// Api
+// @Description: client发起RPC接口请求，对外提供的服务
 func (s baseServer) Api(ctx context.Context, in *proto.Request) (ack *proto.Response, err error) {
 	if in == nil || in.Method == "" {
 		err = errors.New("method is empty")
@@ -40,7 +42,7 @@ func (s baseServer) Api(ctx context.Context, in *proto.Request) (ack *proto.Resp
 	return
 }
 
-// Call client发起请求方法
+// Call client发起请求方法，由RPC内部调用，不对外服务
 func (s baseServer) Call(ctx context.Context, in *Request) (ack *Response, err error) {
 	var out *proto.Response
 	out, err = s.Api(ctx, &proto.Request{Method: in.Method, Data: in.Data})
