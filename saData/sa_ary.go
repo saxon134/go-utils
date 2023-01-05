495732b71363cce9c50a9eb5953a7a79ad0784b1
saxon <jiangruihuang@126.com>
2023-01-05 23:42:45 +0800

    AppendId

--- saData/sa_ary.go
+++ saData/sa_ary.go
@@ -140,3 +140,21 @@ func InArrayFun(ary interface{}, fun func(i int) bool) bool {
 	}
 	return false
 }
+
+//注意：只支持基础类型数据
+func AppendId(ary []int64, id int64) []int64 {
+	if id > 0 {
+		exist := false
+		for _, v := range ary {
+			if v == id {
+				exist = true
+				break
+			}
+		}
+
+		if exist == false {
+			ary = append(ary, id)
+		}
+	}
+	return ary
+}
