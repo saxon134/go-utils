5c0129db6866b3b625640deade7ff377f443ebfe
saxon <jiangruihuang@126.com>
2023-01-26 19:28:20 +0800

    fix

--- saData/sa_float.go
+++ saData/sa_float.go
@@ -5,8 +5,14 @@ import (
 	"strconv"
 )
 
-func Round(f float64, n int) float64 {
+func RoundFloat64(f float64, n int) float64 {
 	floatStr := fmt.Sprintf("%."+strconv.Itoa(n)+"f", f)
 	inst, _ := strconv.ParseFloat(floatStr, 64)
 	return inst
+}
+
+func RoundFloat32(f float32, n int) float32 {
+	floatStr := fmt.Sprintf("%."+strconv.Itoa(n)+"f", f)
+	inst, _ := strconv.ParseFloat(floatStr, 64)
+	return float32(inst)
 }
